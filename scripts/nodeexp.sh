56706ff6a301ae0790ff8ab0dcbdced87bc160ec
shefat <faisalshefat@gmail.com>
2026-06-18 19:10:15 -0400

    updated Node Exporter

--- scripts/nodeexp.sh
+++ scripts/nodeexp.sh
@@ -1,30 +1,18 @@
-#!/bin/bash
 set -e
  
 echo "=============================="
-echo " Prometheus Setup Script"
+echo " Node Exporter Setup Script"
 echo "=============================="
  
-# Update system
-sudo apt update -y
+# Create a dedicated system user for Node Exporter
+sudo useradd --system --no-create-home --shell /bin/false node_exporter
  
-# Create a dedicated system user for Prometheus
-sudo useradd --system --no-create-home --shell /bin/false prometheus
+# Download and install Node Exporter
+NODE_EXPORTER_VERSION="1.6.1"
+wget "https://github.com/prometheus/node_exporter/releases/download/v${NODE_EXPORTER_VERSION}/node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz"
+tar -xvf "node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz"
+sudo mv "node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64/node_exporter" /usr/local/bin/
+rm -rf node_exporter*
  
-# Download and extract Prometheus
-PROM_VERSION="2.47.1"
-wget "https://github.com/prometheus/prometheus/releases/download/v${PROM_VERSION}/prometheus-${PROM_VERSION}.linux-amd64.tar.gz"
-tar -xvf "prometheus-${PROM_VERSION}.linux-amd64.tar.gz"
- 
-# Set up directories
-sudo mkdir -p /data /etc/prometheus
-cd "prometheus-${PROM_VERSION}.linux-amd64/"
-sudo mv prometheus promtool /usr/local/bin/
-sudo mv consoles/ console_libraries/ /etc/prometheus/
-sudo mv prometheus.yml /etc/prometheus/prometheus.yml
-sudo chown -R prometheus:prometheus /etc/prometheus/ /data/
- 
-cd ..
-rm -rf "prometheus-${PROM_VERSION}.linux-amd64.tar.gz" "prometheus-${PROM_VERSION}.linux-amd64"
-prometheus --version
+node_exporter --version
 
